5ca872fdb391dd6b5720e9823e5813049e1da963
Daria Kudriavtseva <situation96@mail.ru>
2023-05-11 16:03:35 +0300

    Описан класс RegistrationPage для высокоуровневых шагов

--- qaguru_10_DK/registration_page.py
+++ qaguru_10_DK/registration_page.py
@@ -1,49 +1,69 @@
 from pathlib import Path
-
 from selene import browser, have, command
-
 import tests
+from qaguru_10_DK.data.users import User
 
 
 class RegistrationPage:
+    def __init__(self):
+        self.google_adds = browser.all("[id^=google_ads][id$=container__]")
+        self.submit = browser.element("#submit")
+
     def open(self):
         browser.open("https://demoqa.com/automation-practice-form")
 
-        browser.all("[id^=google_ads][id$=container__]").with_(timeout=10).wait_until(
+        self.google_adds.with_(timeout=10).wait_until(
             have.size_greater_than_or_equal(3)
         )
-        browser.all("[id^=google_ads][id$=container__]").perform(command.js.remove)
+        self.google_adds.perform(command.js.remove)
 
-    def fill_first_name(self, value):
+    def register(self, user: User):
+        self.__fill_first_name(user.first_name)
+        self.__fill_last_name(user.last_name)
+        self.__fill_user_email(user.email)
+        self.__choose_gender(user.gender)
+        self.__fill_user_number(user.telephone_number)
+        self.__fill_date_of_birth(
+            user.year_of_birth, user.month_of_birth, user.day_of_birth
+        )
+        self.__fill_subjects(user.subjects)
+        self.__choose_hobbies(user.hobbies)
+        self.__upload_picture(user.picture)
+        self.__fill_address(user.address)
+        self.__choose_state(user.state)
+        self.__choose_city(user.city)
+        self.__submit_form()
+
+    def __fill_first_name(self, value):
         browser.element("#firstName").type(value).press_enter()
 
-    def fill_last_name(self, value):
+    def __fill_last_name(self, value):
         browser.element("#lastName").type(value).press_enter()
 
-    def fill_user_email(self, value):
+    def __fill_user_email(self, value):
         browser.element("#userEmail").type(value).press_enter()
 
-    def choose_gender(self, value):
+    def __choose_gender(self, value):
         browser.all("[for^=gender-radio]").element_by(have.text(value)).click()
 
-    def fill_user_number(self, value):
+    def __fill_user_number(self, value):
         browser.element("#userNumber").type(value)
 
-    def fill_date_of_birth(self, year, month, day):
+    def __fill_date_of_birth(self, year, month, day):
         browser.element("#dateOfBirthInput").click()
         browser.element(".react-datepicker__month-select").send_keys(month)
         browser.element(".react-datepicker__year-select").send_keys(year)
         browser.element(f'.react-datepicker__day--0{day}').click()
 
-    def fill_subjects(self, *names):
+    def __fill_subjects(self, names: list):
         for name in names:
             browser.element("#subjectsInput").set_value(name).press_enter()
 
-    def choose_hobbies(self, *values):
+    def __choose_hobbies(self, values: list):
         for value in values:
             browser.all(".custom-checkbox").element_by(have.exact_text(value)).click()
 
-    def upload_picture(self, file_name):
+    def __upload_picture(self, file_name):
         browser.element("#uploadPicture").set_value(
             str(
                 Path(tests.__file__)
@@ -52,52 +72,65 @@ class RegistrationPage:
             )
         )
 
-    def fill_address(self, value):
+    def __fill_address(self, value):
         browser.element("#currentAddress").type(value).press_enter()
 
-    def choose_state(self, value):
+    def __choose_state(self, value):
         browser.element("#state").perform(command.js.scroll_into_view)
         browser.element("#state").click()
         browser.all("[id^=react-select][id*=option]").element_by(
             have.text(value)
         ).click()
 
-    def choose_city(self, value):
+    def __choose_city(self, value):
         browser.element("#city").click()
         browser.all("[id^=react-select][id*=option]").element_by(
             have.text(value)
         ).click()
 
-    def submit_form(self):
-        # browser.driver.execute_script("$('footer').hide()")
-        browser.element("#submit").perform(command.js.scroll_into_view)
-        # browser.driver.execute_script("document.body.style.zoom='60%'")
-        browser.element("#submit").click()
+    def __submit_form(self):
+        self.submit.perform(command.js.scroll_into_view)
+        self.submit.click()
 
-    def should_have_register_info(
-        self,
-        user_name,
-        email,
-        gender,
-        telephone_number,
-        date_of_birth,
-        subjects,
-        hobbies,
-        picture,
-        address,
-        state_city,
-    ):
+    # def should_have_register_info(
+    #     self,
+    #     user_name,
+    #     email,
+    #     gender,
+    #     telephone_number,
+    #     date_of_birth,
+    #     subjects,
+    #     hobbies,
+    #     picture,
+    #     address,
+    #     state_city,
+    # ):
+    #     browser.element(".table").all("td").even.should(
+    #         have.texts(
+    #             user_name,
+    #             email,
+    #             gender,
+    #             telephone_number,
+    #             date_of_birth,
+    #             subjects,
+    #             hobbies,
+    #             picture,
+    #             address,
+    #             state_city,
+    #         )
+    #     )
+    def should_have_register_info(self, user: User):
         browser.element(".table").all("td").even.should(
             have.texts(
-                user_name,
-                email,
-                gender,
-                telephone_number,
-                date_of_birth,
-                subjects,
-                hobbies,
-                picture,
-                address,
-                state_city,
+                user.full_name,
+                user.email,
+                user.gender,
+                user.telephone_number,
+                user.date_of_birth,
+                user.all_subjects,
+                user.all_hobbies,
+                user.picture,
+                user.address,
+                user.state_city,
             )
         )
