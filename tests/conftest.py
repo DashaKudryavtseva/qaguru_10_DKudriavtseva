4fa6265453e453f69753dc1d0b7b81cfb4f4b427
Daria Kudriavtseva <situation96@mail.ru>
2023-05-11 16:02:19 +0300

    рефакторинг конфигурации

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,9 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def browser_configuration():
     browser.config.browser_name = "chrome"
-    # browser.config.hold_browser_open = True
     browser.config.window_height = 1080
     browser.config.window_width = 720
 
